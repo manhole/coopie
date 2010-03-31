a0d866f86ee4b42e5e17c1d545ed32e737749d39
manhole <manholex@gmail.com>
2010-03-31 16:04:27 +0000

    CSVヘッダがJavaBeansのプロパティ名と異なる場合でも、CSVを読めるようにした。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@19 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/test/java/jp/sourceforge/hotchpotch/coopie/csv/BeanCsvWriterTest.java
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/csv/BeanCsvWriterTest.java
@@ -11,12 +11,14 @@ import java.util.List;
 
 import jp.sourceforge.hotchpotch.coopie.Closable;
 import jp.sourceforge.hotchpotch.coopie.csv.BeanCsvReaderTest.AaaBean;
+import jp.sourceforge.hotchpotch.coopie.csv.BeanCsvReaderTest.BeanColumnDesc;
 import jp.sourceforge.hotchpotch.coopie.csv.BeanCsvReaderTest.CsvSetting;
 
 import org.junit.Test;
 import org.t2framework.commons.meta.BeanDesc;
 import org.t2framework.commons.meta.BeanDescFactory;
 import org.t2framework.commons.meta.PropertyDesc;
+import org.t2framework.commons.util.CollectionsUtil;
 import org.t2framework.commons.util.ReaderUtil;
 import org.t2framework.commons.util.ResourceUtil;
 
@@ -104,15 +106,105 @@ public class BeanCsvWriterTest {
 
     public static class ColumnLayout {
 
-        private String[] names;
+        private BeanColumnDesc[] columnDescs;
+        private List<ColumnName> columnNames;
 
-        public String[] getNames() {
+        public BeanColumnDesc[] getColumnDescs() {
+            if (columnDescs == null) {
+                columnDescs = new BeanColumnDesc[columnNames.size()];
+                int i = 0;
+                for (final ColumnName columnName : columnNames) {
+                    final BeanColumnDesc columnDesc = new BeanColumnDesc();
+                    columnDesc.setName(columnName);
+                    columnDescs[i] = columnDesc;
+                    i++;
+                }
+            }
+            return columnDescs;
+        }
+
+        public void setColumnDescs(final BeanColumnDesc[] columnDescs) {
+            this.columnDescs = columnDescs;
+        }
+
+        public ColumnName[] getNames() {
+            final ColumnName[] names = new ColumnName[columnDescs.length];
+            for (int i = 0; i < columnDescs.length; i++) {
+                final BeanColumnDesc columnDesc = columnDescs[i];
+                names[i] = columnDesc.getName();
+            }
+
             return names;
         }
 
         public void setNames(final String[] names) {
-            this.names = names;
+            columnDescs = new BeanColumnDesc[names.length];
+            for (int i = 0; i < names.length; i++) {
+                final String name = names[i];
+                final BeanColumnDesc columnDesc = new BeanColumnDesc();
+                columnDesc.setName(new ColumnName(name));
+                columnDescs[i] = columnDesc;
+            }
         }
+
+        public BeanColumnDesc getColumnDescByAlias(final String alias) {
+            for (final BeanColumnDesc cd : columnDescs) {
+                if (cd.getName().equals(alias)) {
+                    return cd;
+                }
+            }
+            // TODO 例外クラスにする
+            throw new RuntimeException();
+        }
+
+        public void addAlias(final String alias, final String propertyName) {
+            // TODO Auto-generated method stub
+            final ColumnName columnName = new ColumnName();
+            columnName.setLabel(alias);
+            columnName.setName(propertyName);
+            if (columnNames == null) {
+                columnNames = CollectionsUtil.newArrayList();
+            }
+            columnNames.add(columnName);
+        }
+
+    }
+
+    public static class ColumnName {
+
+        public ColumnName() {
+        }
+
+        public ColumnName(final String labelAndName) {
+            setLabel(labelAndName);
+            setName(labelAndName);
+        }
+
+        /**
+         * CSVの項目名
+         */
+        private String label;
+
+        public String getLabel() {
+            return label;
+        }
+
+        public void setLabel(final String label) {
+            this.label = label;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(final String name) {
+            this.name = name;
+        }
+
+        /**
+         * Beanのプロパティ名
+         */
+        private String name;
 
     }
 
@@ -152,10 +244,10 @@ public class BeanCsvWriterTest {
         public BeanCsvWriter(final Class<T> beanClass,
             final ColumnLayout columnLayout) {
             beanDesc = BeanDescFactory.getBeanDesc(beanClass);
-            final String[] names = columnLayout.getNames();
+            final ColumnName[] names = columnLayout.getNames();
             propertyDescs = new PropertyDesc[names.length];
             for (int i = 0; i < names.length; i++) {
-                final String name = names[i];
+                final String name = names[i].getName();
                 final PropertyDesc<T> pd = beanDesc.getPropertyDesc(name);
                 propertyDescs[i] = pd;
             }
