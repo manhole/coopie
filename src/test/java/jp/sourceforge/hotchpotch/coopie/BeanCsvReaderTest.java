ea45058ba673f8a58524377eccd892b494cda69e
manhole <manholex@gmail.com>
2010-03-31 08:56:04 +0000

    単純なJavaBeansへCSVを読めるようにした。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@13 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

---
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/BeanCsvReaderTest.java
@@ -0,0 +1,267 @@
+package jp.sourceforge.hotchpotch.coopie;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.t2framework.commons.exception.IORuntimeException;
+import org.t2framework.commons.meta.BeanDesc;
+import org.t2framework.commons.meta.BeanDescFactory;
+import org.t2framework.commons.meta.PropertyDesc;
+import org.t2framework.commons.util.ResourceUtil;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+/*
+ * CSVの設定としては、
+ * - JavaBeanクラス
+ * - 改行文字(writerのみ)
+ * - 区切り文字: tabかカンマか
+ * - CSVヘッダの有無
+ * - CSV項目の順序
+ *   ヘッダがある場合は、read時は不要。
+ * - ""のときに、nullにするか、""にするか。
+ */
+public class BeanCsvReaderTest {
+
+    private static final Logger logger = LoggerFactory.getLogger();
+
+    @Test
+    public void read1() throws Throwable {
+        // ## Arrange ##
+        final InputStream is = ResourceUtil.getResourceAsStream(
+            BeanCsvReaderTest.class.getName() + "-1", "tsv");
+
+        final BeanCsvReader<AaaBean> csvReader = new BeanCsvReader<AaaBean>(
+            AaaBean.class);
+
+        // ## Act ##
+        csvReader.open(new InputStreamReader(is, "UTF-8"));
+        //logger.debug(ReaderUtil.readText(new InputStreamReader(is, "UTF-8")));
+
+        final AaaBean bean = new AaaBean();
+        csvReader.fetch(bean);
+
+        // ## Assert ##
+        logger.debug(bean.toString());
+        assertEquals("あ1", bean.getAaa());
+        assertEquals("い1", bean.getBbb());
+        assertEquals("う1", bean.getCcc());
+
+        csvReader.fetch(bean);
+        logger.debug(bean.toString());
+        assertEquals("あ2", bean.getAaa());
+        assertEquals("い2", bean.getBbb());
+        assertEquals("う2", bean.getCcc());
+
+        csvReader.fetch(bean);
+        logger.debug(bean.toString());
+        assertEquals("あ3", bean.getAaa());
+        assertEquals("い3", bean.getBbb());
+        assertEquals("う3", bean.getCcc());
+
+        csvReader.close();
+    }
+
+    public static class CsvSetting {
+
+        public static final char TAB = '\t';
+        public static final char COMMA = ',';
+        public static final char DOUBLE_QUOTE = '\"';
+        public static final String CRLF = "\r\n";
+
+        /**
+         * 要素区切り文字。
+         * 未指定の場合はタブです。
+         */
+        private char elementSeparator = TAB;
+        /**
+         * 要素をクォートする文字。
+         * 未指定の場合はダブルクォート(二重引用符)です。
+         */
+        private char quoteMark = DOUBLE_QUOTE;
+
+        /**
+         * 改行文字。
+         * 未指定の場合はCRLFです。
+         * 
+         * CsvWriterを使う場合は、何らかの値が設定されている必要があります。
+         * (CRLFのままでもOKです)
+         * 
+         * CsvReaderを使う場合は、未設定のままで構いません。
+         */
+        private String lineSeparator = CRLF;
+
+        public String getLineSeparator() {
+            return lineSeparator;
+        }
+
+        public void setLineSeparator(final String lineSeparator) {
+            this.lineSeparator = lineSeparator;
+        }
+
+        public char getQuoteMark() {
+            return quoteMark;
+        }
+
+        public void setQuoteMark(final char quoteMark) {
+            this.quoteMark = quoteMark;
+        }
+
+        public char getElementSeparator() {
+            return elementSeparator;
+        }
+
+        public void setElementSeparator(final char elementSeparator) {
+            this.elementSeparator = elementSeparator;
+        }
+
+    }
+
+    public static class BeanCsvReader<T> implements Closable {
+
+        private CsvSetting csvSetting = new CsvSetting();
+
+        public CsvSetting getCsvSetting() {
+            return csvSetting;
+        }
+
+        public void setCsvSetting(final CsvSetting csvSetting) {
+            this.csvSetting = csvSetting;
+        }
+
+        private final Class<T> beanClass;
+        /**
+         * BeanCsvReader close時に、Readerを一緒にcloseする場合はtrue。
+         */
+        private boolean closeReader = true;
+        private Boolean hasNext = null;
+        private CSVReader csvReader;
+        protected boolean closed = true;
+        private final BeanDesc<T> beanDesc;
+        private PropertyDesc<T>[] propertyDescs;
+        private String[] nextLine;
+
+        public void fetch(final T bean) {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            hasNext = null;
+
+            final String[] line;
+            if (nextLine != null) {
+                line = nextLine;
+            } else {
+                throw new AssertionError();
+            }
+            for (int i = 0; i < line.length; i++) {
+                final String elem = line[i];
+                final PropertyDesc<T> pd = propertyDescs[i];
+                pd.setValue(bean, elem);
+            }
+        }
+
+        public void open(final Reader reader) {
+            csvReader = new CSVReader(reader, csvSetting.getElementSeparator(),
+                csvSetting.getQuoteMark());
+            closed = false;
+
+            final String[] header = readLine();
+            propertyDescs = new PropertyDesc[header.length];
+            for (int i = 0; i < header.length; i++) {
+                final PropertyDesc<T> pd = beanDesc.getPropertyDesc(header[i]);
+                propertyDescs[i] = pd;
+            }
+        }
+
+        private String[] readLine() {
+            try {
+                return csvReader.readNext();
+            } catch (final IOException e) {
+                throw new IORuntimeException(e);
+            }
+        }
+
+        private boolean hasNext() {
+            if (hasNext != null) {
+                return hasNext.booleanValue();
+            }
+            nextLine = readLine();
+
+            if (nextLine == null) {
+                hasNext = Boolean.FALSE;
+            } else {
+                hasNext = Boolean.TRUE;
+            }
+            return hasNext.booleanValue();
+        }
+
+        public BeanCsvReader(final Class<T> beanClass) {
+            this.beanClass = beanClass;
+            beanDesc = BeanDescFactory.getBeanDesc(beanClass);
+        }
+
+        public void setCloseReader(final boolean closeReader) {
+            this.closeReader = closeReader;
+        }
+
+        @Override
+        public boolean isClosed() {
+            return closed;
+        }
+
+        @Override
+        public void close() throws IOException {
+            closed = true;
+            // TODO
+            csvReader.close();
+        }
+
+    }
+
+    public static class AaaBean {
+
+        private String aaa;
+        private String bbb;
+        private String ccc;
+
+        public String getAaa() {
+            return aaa;
+        }
+
+        public void setAaa(final String aaa) {
+            this.aaa = aaa;
+        }
+
+        public String getBbb() {
+            return bbb;
+        }
+
+        public void setBbb(final String bbb) {
+            this.bbb = bbb;
+        }
+
+        public String getCcc() {
+            return ccc;
+        }
+
+        public void setCcc(final String ccc) {
+            this.ccc = ccc;
+        }
+
+        private final ToStringFormat toStringFormat = new ToStringFormat();
+
+        @Override
+        public String toString() {
+            return toStringFormat.format(this);
+        }
+
+    }
+
+}
