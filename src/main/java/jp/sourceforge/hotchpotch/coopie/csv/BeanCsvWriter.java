00fc3ff5b61b8ce6bf358e0312f7d9bfa2fd8fbc
manhole <manholex@gmail.com>
2010-04-02 09:06:39 +0000

    BeanCsvWriterの親としてAbstractCsvWriterを設けた。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@41 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/main/java/jp/sourceforge/hotchpotch/coopie/csv/BeanCsvWriter.java
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/BeanCsvWriter.java
@@ -1,29 +1,9 @@
 package jp.sourceforge.hotchpotch.coopie.csv;
 
-import java.io.IOException;
-import java.io.Writer;
-
 import jp.sourceforge.hotchpotch.coopie.Closable;
-import au.com.bytecode.opencsv.CSVWriter;
 
-public class BeanCsvWriter<T> implements Closable {
+public class BeanCsvWriter<T> extends AbstractCsvWriter<T> implements Closable {
 
-    private CsvSetting csvSetting = new CsvSetting();
-
-    public CsvSetting getCsvSetting() {
-        return csvSetting;
-    }
-
-    public void setCsvSetting(final CsvSetting csvSetting) {
-        this.csvSetting = csvSetting;
-    }
-
-    protected boolean closed = true;
-    /**
-     * BeanCsvWriter close時に、Writerを一緒にcloseする場合はtrue。
-     */
-    private boolean closeWriter = true;
-    private CSVWriter csvWriter;
     private final BeanColumnLayout<T> columnLayout;
 
     public BeanCsvWriter(final Class<T> beanClass) {
@@ -34,45 +14,13 @@ public class BeanCsvWriter<T> implements Closable {
         this.columnLayout = columnLayout;
     }
 
-    public void open(final Writer writer) {
-        csvWriter = new CSVWriter(writer, csvSetting.getElementSeparator(),
-            csvSetting.getQuoteMark(), csvSetting.getLineSeparator());
-        writeHeader();
-        closed = false;
-    }
-
-    private void writeHeader() {
-        final ColumnName[] names = columnLayout.getNames();
-        final String[] line = new String[names.length];
-        int i = 0;
-        for (final ColumnName name : names) {
-            line[i] = name.getLabel();
-            i++;
-        }
-        csvWriter.writeNext(line);
-    }
-
-    public void write(final T bean) {
-        final String[] line = columnLayout.getValues(bean);
-        csvWriter.writeNext(line);
+    protected String[] getValues(final T bean) {
+        return columnLayout.getValues(bean);
     }
 
     @Override
-    public boolean isClosed() {
-        return closed;
-    }
-
-    @Override
-    public void close() throws IOException {
-        closed = true;
-        if (closeWriter) {
-            csvWriter.close();
-            csvWriter = null;
-        }
-    }
-
-    public void setCloseWriter(final boolean closeWriter) {
-        this.closeWriter = closeWriter;
+    protected ColumnName[] getColumnNames() {
+        return columnLayout.getNames();
     }
 
 }
