556f7e37a4662b039313c9c3917739fd3150329b
manhole <manhole@ac04e864-1e22-0410-bbb8-8cc9166c4c4a>
2010-10-28 16:11:27 +0000

    sha1
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@119 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

---
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/Sha1Test.java
@@ -0,0 +1,26 @@
+package jp.sourceforge.hotchpotch.coopie;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.t2framework.commons.util.ResourceUtil;
+
+public class Sha1Test {
+
+    @Test
+    public void test1() throws Throwable {
+        // ## Arrange ##
+        final File file = ResourceUtil.getResourceAsFile(Sha1Test.class
+            .getPackage().getName().replace('.', '/')
+            + "/coopie-0.1.0-20100708.043157-1.pom");
+
+        // ## Act ##
+        final String actual = new Sha1().digest(file);
+
+        // ## Assert ##
+        assertEquals("e31ef5b931e16ce308cbad3b5c96360e1a1ea619", actual);
+    }
+
+}
