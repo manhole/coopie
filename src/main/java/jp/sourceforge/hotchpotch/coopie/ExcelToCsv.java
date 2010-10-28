f8beb2f6142ceeadd04d7256d9a05226d36d4182
manhole <manholex@gmail.com>
2010-10-28 17:35:43 +0000

    Excelからtsvを作成できるようにした
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@125 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

---
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/ExcelToCsv.java
@@ -0,0 +1,87 @@
+package jp.sourceforge.hotchpotch.coopie;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import jp.sourceforge.hotchpotch.coopie.csv.CsvSetting;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.slf4j.Logger;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+public class ExcelToCsv {
+
+    private static final Logger logger = LoggerFactory.getLogger();
+    private final FileOperation files = new FileOperation();
+    private static final String TSV_EXTENSION = ".tsv";
+
+    public void writeTsv(final File file) throws IOException {
+        logger.debug("file={}", file.getAbsolutePath());
+        if (!file.exists()) {
+            throw new IllegalArgumentException("not exist:"
+                    + file.getAbsolutePath());
+
+        }
+
+        final FileResource fr = files.getFileResource(file);
+        final File tsvFile = files.createFile(file.getParentFile(),
+                fr.getPrefix() + TSV_EXTENSION);
+
+        final HSSFWorkbook workbook = new HSSFWorkbook(
+                files.openBufferedInputStream(file));
+        final HSSFSheet sheet = workbook.getSheetAt(0);
+
+        final CSVWriter csvWriter = new CsvSetting().openWriter(files
+                .openBufferedWriter(tsvFile));
+
+        final int lastRowNum = sheet.getLastRowNum();
+        for (int rowNo = 0; rowNo <= lastRowNum; rowNo++) {
+            final HSSFRow row = sheet.getRow(rowNo);
+            final short lastCellNum = row.getLastCellNum();
+            final String[] line = new String[lastCellNum];
+            for (int colNo = 0; colNo < lastCellNum; colNo++) {
+                final HSSFCell cell = row.getCell(colNo);
+                final String v = getValueAsString(cell);
+                line[colNo] = v;
+            }
+            logger.debug("row: " + Arrays.asList(line));
+            csvWriter.writeNext(line);
+        }
+
+        csvWriter.close();
+    }
+
+    private String getValueAsString(final HSSFCell cell) {
+        if (cell == null) {
+            return null;
+        }
+        switch (cell.getCellType()) {
+        case HSSFCell.CELL_TYPE_NUMERIC:
+            final double v = cell.getNumericCellValue();
+            if (isInt(v)) {
+                return Integer.toString((int) v);
+            }
+            return Double.toString(v);
+        case HSSFCell.CELL_TYPE_BOOLEAN:
+            final boolean b = cell.getBooleanCellValue();
+            return Boolean.toString(b);
+        case HSSFCell.CELL_TYPE_STRING:
+        default:
+            final HSSFRichTextString richStringCellValue = cell
+                    .getRichStringCellValue();
+            final String value = richStringCellValue.getString();
+            return value;
+        }
+    }
+
+    private boolean isInt(final double numericValue) {
+        return ((int) numericValue) == numericValue;
+    }
+
+}
---
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/ExcelToCsvTest.java
@@ -0,0 +1,77 @@
+package jp.sourceforge.hotchpotch.coopie;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import jp.sourceforge.hotchpotch.coopie.csv.CsvReader;
+import jp.sourceforge.hotchpotch.coopie.csv.MapCsvLayout;
+import junitx.framework.ListAssert;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.t2framework.commons.util.CollectionsUtil;
+import org.t2framework.commons.util.ResourceUtil;
+
+public class ExcelToCsvTest {
+
+    private static final Logger logger = LoggerFactory.getLogger();
+    private final FileOperation files = new FileOperation();
+
+    @Test
+    public void test1() throws Exception {
+        // ## Arrange ##
+        final File inFile = ResourceUtil.getResourceAsFile(Sha1Test.class
+                .getPackage().getName().replace('.', '/')
+                + "/ExcelToCsvTest-1.xls");
+        final File outFile = new File(inFile.getParentFile(),
+                "ExcelToCsvTest-1.tsv");
+        logger.debug("outFile={}", outFile);
+
+        files.delete(outFile);
+
+        // ## Act ##
+        final ExcelToCsv excelToCsv = new ExcelToCsv();
+        excelToCsv.writeTsv(inFile);
+
+        // ## Assert ##
+        assertEquals(true, outFile.exists());
+
+        final List<Map<String, String>> actList;
+        {
+            final MapCsvLayout layout = new MapCsvLayout();
+            layout.setWithHeader(true);
+            final CsvReader<Map<String, String>> actualCsvReader = layout
+                    .openReader(files.openBufferedReader(outFile));
+            actList = readAll(actualCsvReader);
+        }
+
+        final List<Map<String, String>> exList;
+        {
+            final MapCsvLayout layout = new MapCsvLayout();
+            layout.setWithHeader(true);
+
+            final File expectedFile = ResourceUtil
+                    .getResourceAsFile(Sha1Test.class.getPackage().getName()
+                            .replace('.', '/')
+                            + "/ExcelToCsvTest-1-expected.xls.tsv");
+            final CsvReader<Map<String, String>> expectedCsvReader = layout
+                    .openReader(files.openBufferedReader(expectedFile));
+            exList = readAll(expectedCsvReader);
+        }
+        ListAssert.assertEquals(exList, actList);
+    }
+
+    private <T> List<T> readAll(final CsvReader<T> reader) {
+        final List<T> list = CollectionsUtil.newArrayList();
+        while (reader.hasNext()) {
+            final T bean = reader.read();
+            list.add(bean);
+        }
+        IOUtil.closeNoException(reader);
+        return list;
+    }
+
+}
