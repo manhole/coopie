9a3acd9d6ff0ce2feb48421addbbcb79751afd6b
manhole <manholex@gmail.com>
2010-11-03 08:17:13 +0000

    DefaultCsvWriterをAbstractCsvWriterへ分割した。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@150 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/main/java/jp/sourceforge/hotchpotch/coopie/csv/DefaultCsvWriter.java
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/DefaultCsvWriter.java
@@ -1,82 +1,13 @@
 package jp.sourceforge.hotchpotch.coopie.csv;
 
-import java.io.IOException;
 import java.io.Writer;
 
-import jp.sourceforge.hotchpotch.coopie.Closable;
-
-class DefaultCsvWriter<T> implements Closable, CsvWriter<T> {
-
-    private boolean closed = true;
-
-    /**
-     * CsvWriter close時に、Writerを一緒にcloseする場合はtrue。
-     */
-    private boolean closeWriter = true;
-
-    private boolean firstRecord = true;
-
-    protected CsvElementWriter csvWriter;
-
-    private boolean writtenHeader;
+class DefaultCsvWriter<T> extends AbstractCsvWriter<T> {
 
     private CsvSetting csvSetting = new CsvSetting();
 
-    protected RecordDesc<T> recordDesc;
-
     public DefaultCsvWriter(final RecordDesc<T> recordDesc) {
-        this.recordDesc = recordDesc;
-    }
-
-    public void open(final Writer writer) {
-        csvWriter = csvSetting.openWriter(writer);
-        closed = false;
-    }
-
-    /*
-     * 1レコード目を出力するときに、このメソッドが呼ばれる。
-     */
-    protected void writeHeader(final T bean) {
-        final ColumnName[] names = recordDesc.getColumnNames();
-        final String[] line = new String[names.length];
-        int i = 0;
-        for (final ColumnName name : names) {
-            line[i] = name.getLabel();
-            i++;
-        }
-        csvWriter.writeRecord(line);
-    }
-
-    @Override
-    public void write(final T bean) {
-        if (firstRecord) {
-            firstRecord = false;
-            recordDesc = recordDesc.setupByBean(bean);
-        }
-        if (!writtenHeader) {
-            writeHeader(bean);
-            writtenHeader = true;
-        }
-        final String[] line = recordDesc.getValues(bean);
-        csvWriter.writeRecord(line);
-    }
-
-    @Override
-    public boolean isClosed() {
-        return closed;
-    }
-
-    @Override
-    public void close() throws IOException {
-        closed = true;
-        if (closeWriter) {
-            csvWriter.close();
-            csvWriter = null;
-        }
-    }
-
-    public void setCloseWriter(final boolean closeWriter) {
-        this.closeWriter = closeWriter;
+        super(recordDesc);
     }
 
     public CsvSetting getCsvSetting() {
@@ -85,6 +16,11 @@ class DefaultCsvWriter<T> implements Closable, CsvWriter<T> {
 
     public void setCsvSetting(final CsvSetting csvSetting) {
         this.csvSetting = csvSetting;
+    }
+
+    public void open(final Writer writer) {
+        csvWriter = csvSetting.openWriter(writer);
+        closed = false;
     }
 
 }
