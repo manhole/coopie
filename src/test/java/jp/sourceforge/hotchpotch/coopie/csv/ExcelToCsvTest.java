c0dff6419f97fc6cf55793a6fdaa458216e90cc0
manhole <manholex@gmail.com>
2010-12-09 08:06:17 +0000

    複数シートのexcelファイルからTSVファイルを作成できるようにした。 TSVファイル名にはシート名を付加する。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@187 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/test/java/jp/sourceforge/hotchpotch/coopie/csv/ExcelToCsvTest.java
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/csv/ExcelToCsvTest.java
@@ -11,6 +11,8 @@ import jp.sourceforge.hotchpotch.coopie.IOUtil;
 import jp.sourceforge.hotchpotch.coopie.LoggerFactory;
 import junitx.framework.ListAssert;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.t2framework.commons.util.CollectionsUtil;
@@ -20,6 +22,17 @@ public class ExcelToCsvTest {
 
     private static final Logger logger = LoggerFactory.getLogger();
     private final FileOperation files = new FileOperation();
+    private File rootDir;
+
+    @Before
+    public void setUp() throws Throwable {
+        rootDir = files.createTempDir();
+    }
+
+    @After
+    public void tearDown() {
+        files.delete(rootDir);
+    }
 
     @Test
     public void test1() throws Exception {
@@ -69,6 +82,42 @@ public class ExcelToCsvTest {
                 ExcelToCsvTest.class.getName() + "-2-expected", "tsv");
 
         assertCsvEquals(expectedFile, outFile);
+    }
+
+    /**
+     * 複数シートを持つ場合
+     */
+    @Test
+    public void test3() throws Exception {
+        // ## Arrange ##
+        final File testFile = ResourceUtil.getResourceAsFile(
+                ExcelToCsvTest.class.getName() + "-3", "xls");
+
+        final File inFile = new File(rootDir, testFile.getName());
+        files.copy(testFile, inFile);
+
+        final File outFile1 = new File(rootDir, "ExcelToCsvTest-3-Sheet1.tsv");
+        final File outFile2 = new File(rootDir, "ExcelToCsvTest-3-Sheet2.tsv");
+        final File outFile3 = new File(rootDir, "ExcelToCsvTest-3-Sheet3.tsv");
+        logger.debug("outFile={}", outFile1);
+        assertEquals(1, rootDir.list().length);
+
+        // ## Act ##
+        final ExcelToCsv excelToCsv = new ExcelToCsv();
+        excelToCsv.writeTsv(inFile);
+
+        // ## Assert ##
+        assertEquals(true, outFile1.exists());
+        assertEquals(false, outFile2.exists());
+        assertEquals(true, outFile3.exists());
+        assertEquals(3, rootDir.list().length);
+
+        final File expectedFile1 = ResourceUtil
+                .getResourceAsFile(ExcelToCsvTest.class.getPackage().getName()
+                        .replace('.', '/')
+                        + "/" + "ExcelToCsvTest-3-expected1.tsv");
+
+        assertCsvEquals(expectedFile1, outFile1);
     }
 
     private void assertCsvEquals(final File expectedFile, final File actualFile) {
