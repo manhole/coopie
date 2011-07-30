03ff0a71c3cac39b3404ca2564c71623f8db13f9
manhole <manholex@gmail.com>
2011-07-30 14:47:35 +0000

    Excel出力結果をassertするのに、もう少し低レベルなAPIを使うことにした。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@240 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/test/java/jp/sourceforge/hotchpotch/coopie/csv/BeanExcelWriterTest.java
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/csv/BeanExcelWriterTest.java
@@ -1,6 +1,9 @@
 package jp.sourceforge.hotchpotch.coopie.csv;
 
+import static jp.sourceforge.hotchpotch.coopie.VarArgs.a;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -58,39 +61,14 @@ public class BeanExcelWriterTest {
                 baos.toByteArray()));
         assertEquals(1, book.getNumberOfSheets());
 
-        {
-            final BeanExcelLayout<AaaBean> layout2 = new BeanExcelLayout<AaaBean>(
-                    AaaBean.class);
-
-            final CsvReader<AaaBean> csvReader = layout2
-                    .openReader(new ByteArrayInputStream(baos.toByteArray()));
-            {
-                assertEquals(true, csvReader.hasNext());
-                final AaaBean record = csvReader.read();
-                assertEquals("あ1", record.getAaa());
-                assertEquals("い1", record.getBbb());
-                assertEquals("う1", record.getCcc());
-            }
-            {
-                assertEquals(true, csvReader.hasNext());
-                final AaaBean record = csvReader.read();
-                assertEquals("あ2", record.getAaa());
-                assertEquals("い2", record.getBbb());
-                assertEquals("う2", record.getCcc());
-            }
-            {
-                assertEquals(true, csvReader.hasNext());
-                final AaaBean record = csvReader.read();
-                assertEquals("あ3", record.getAaa());
-                assertEquals("い3", record.getBbb());
-                assertEquals("う3", record.getCcc());
-            }
-
-            assertEquals(false, csvReader.hasNext());
-
-            csvReader.close();
-        }
-
+        final DefaultExcelReader.PoiSheetReader reader = new DefaultExcelReader.PoiSheetReader(
+                book, book.getSheetAt(0));
+        assertArrayEquals(a("aaa", "ccc", "bbb"), reader.readRecord());
+        assertArrayEquals(a("あ1", "う1", "い1"), reader.readRecord());
+        assertArrayEquals(a("あ2", "う2", "い2"), reader.readRecord());
+        assertArrayEquals(a("あ3", "う3", "い3"), reader.readRecord());
+        assertNull(reader.readRecord());
+        reader.close();
     }
 
 }
