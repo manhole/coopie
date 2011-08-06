bf3461677d2c8dabdad73e99792d7453b75b078f
manhole <manholex@gmail.com>
2011-08-06 14:37:22 +0000

    Mapをbeanとして、固定長ファイルを書くテスト
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@281 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

---
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/csv/MapFixedLengthWriterTest.java
@@ -0,0 +1,214 @@
+package jp.sourceforge.hotchpotch.coopie.csv;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.Map;
+
+import jp.sourceforge.hotchpotch.coopie.LoggerFactory;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.t2framework.commons.util.CollectionsUtil;
+import org.t2framework.commons.util.ReaderUtil;
+import org.t2framework.commons.util.ResourceUtil;
+
+public class MapFixedLengthWriterTest {
+
+    private static final Logger logger = LoggerFactory.getLogger();
+
+    /**
+     * 出力できること。
+     * データは右側に寄っていること。
+     */
+    @Test
+    public void write1() throws Throwable {
+        // ## Arrange ##
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 5);
+                setup.column("bbb", 5, 12);
+                setup.column("ccc", 12, 20);
+            }
+        });
+
+        // ## Act ##
+        final StringWriter writer = new StringWriter();
+        final CsvWriter<Map<String, String>> csvWriter = layout
+                .openWriter(writer);
+
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        bean.put("aaa", "あ1");
+        bean.put("bbb", "い1");
+        bean.put("ccc", "う1");
+        csvWriter.write(bean);
+
+        bean.put("aaa", "あ2");
+        bean.put("bbb", "い2");
+        bean.put("ccc", "う2");
+        csvWriter.write(bean);
+
+        csvWriter.close();
+
+        // ## Assert ##
+        final String actual = writer.toString();
+
+        final InputStream is = ResourceUtil.getResourceAsStream(
+        BeanFixedLengthWriterTest.class.getName() + "-1", "tsv");
+        final String expected = ReaderUtil.readText(new InputStreamReader(is,
+                "UTF-8"));
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * カラム順を設定できること。
+     */
+    @Test
+    public void write2() throws Throwable {
+        // ## Arrange ##
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 5);
+                setup.column("ccc", 5, 12);
+                setup.column("bbb", 12, 20);
+            }
+        });
+
+        // ## Act ##
+        final StringWriter writer = new StringWriter();
+        final CsvWriter<Map<String, String>> csvWriter = layout
+                .openWriter(writer);
+
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        bean.put("aaa", "あ1");
+        bean.put("bbb", "い1");
+        bean.put("ccc", "う1");
+        csvWriter.write(bean);
+
+        bean.put("aaa", "あ2");
+        bean.put("bbb", "い2");
+        bean.put("ccc", "う2");
+        csvWriter.write(bean);
+
+        bean.put("aaa", "あ3");
+        bean.put("bbb", "い3");
+        bean.put("ccc", "う3");
+        csvWriter.write(bean);
+
+        csvWriter.close();
+
+        // ## Assert ##
+        final String actual = writer.toString();
+
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-1", "tsv");
+        final String expected = ReaderUtil.readText(new InputStreamReader(is,
+                "UTF-8"));
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * 空白項目がある場合。
+     * 
+     * CSVでは""はnullとして扱い、" "は" "として扱うが、
+     * 固定長では""も" "もnullとし、スペースで埋める。
+     */
+    @Test
+    public void write4() throws Throwable {
+        // ## Arrange ##
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 7);
+                setup.column("bbb", 7, 14);
+                setup.column("ccc", 14, 20);
+            }
+        });
+
+        // ## Act ##
+        final StringWriter writer = new StringWriter();
+        final CsvWriter<Map<String, String>> csvWriter = layout
+                .openWriter(writer);
+
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        bean.put("aaa", "あ1");
+        bean.put("bbb", "い1");
+        bean.put("ccc", " ");
+        csvWriter.write(bean);
+
+        bean.put("aaa", null);
+        bean.put("bbb", "い2");
+        bean.put("ccc", null);
+        csvWriter.write(bean);
+
+        csvWriter.close();
+
+        // ## Assert ##
+        final String actual = writer.toString();
+
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-4-2", "tsv");
+        final String expected = ReaderUtil.readText(new InputStreamReader(is,
+                "UTF-8"));
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * ファイルヘッダが無い場合。
+     * 
+     * ※これが通常の固定長ファイル
+     */
+    @Test
+    public void write_noheader() throws Throwable {
+        // ## Arrange ##
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("ccc", 0, 6);
+                setup.column("aaa", 6, 12);
+                setup.column("bbb", 12, 20);
+            }
+        });
+        layout.setWithHeader(false);
+
+        // ## Act ##
+        final StringWriter writer = new StringWriter();
+        final CsvWriter<Map<String, String>> csvWriter = layout
+                .openWriter(writer);
+
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        bean.put("aaa", "あ1");
+        bean.put("bbb", "い1");
+        bean.put("ccc", "う1");
+        csvWriter.write(bean);
+
+        bean.put("aaa", "あ2");
+        bean.put("bbb", "い2");
+        bean.put("ccc", "う2");
+        csvWriter.write(bean);
+
+        csvWriter.close();
+
+        // ## Assert ##
+        final String actual = writer.toString();
+
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-3", "tsv");
+        final String expected = ReaderUtil.readText(new InputStreamReader(is,
+                "UTF-8"));
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * TODO 定義した長さよりも実際のデータが長い場合
+     */
+
+}
