12ce5a7d557cfbe2605b511aa1c4c447329e8c6d
manhole <manholex@gmail.com>
2011-08-06 14:37:19 +0000

    Mapをbeanとして、固定長ファイルを読めるようにしました
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@280 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

---
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/MapFixedLengthLayout.java
@@ -0,0 +1,78 @@
+package jp.sourceforge.hotchpotch.coopie.csv;
+
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Map;
+
+public class MapFixedLengthLayout extends
+        AbstractFixedLengthLayout<Map<String, String>> implements
+        CsvLayout<Map<String, String>> {
+
+    @Override
+    public CsvReader<Map<String, String>> openReader(final Reader reader) {
+        final RecordDesc<Map<String, String>> rd = getRecordDesc();
+        final ElementSetting es = getElementSetting();
+        final DefaultCsvReader<Map<String, String>> r = new DefaultCsvReader<Map<String, String>>(
+                rd);
+        r.setWithHeader(withHeader);
+        r.setElementSetting(es);
+        // TODO openで例外時にcloseすること
+        r.open(reader);
+        return r;
+    }
+
+    @Override
+    public CsvWriter<Map<String, String>> openWriter(final Writer writer) {
+        final RecordDesc<Map<String, String>> rd = getRecordDesc();
+        final ElementSetting es = getElementSetting();
+        final DefaultCsvWriter<Map<String, String>> w = new DefaultCsvWriter<Map<String, String>>(
+                rd);
+        w.setWithHeader(withHeader);
+        w.setElementSetting(es);
+        // TODO openで例外時にcloseすること
+        w.open(writer);
+        return w;
+    }
+
+    @Override
+    protected FixedLengthRecordDescSetup getRecordDescSetup() {
+        return new MapFixedLengthColumnSetup();
+    }
+
+    private static class MapFixedLengthColumnSetup extends
+            AbstractFixedLengthColumnSetup<Map<String, String>> {
+
+        private FixedLengthRecordDesc<Map<String, String>> fixedLengthRecordDesc;
+
+        @Override
+        public RecordDesc<Map<String, String>> getRecordDesc() {
+            buildIfNeed();
+            return fixedLengthRecordDesc;
+        }
+
+        @Override
+        public ElementSetting getElementSetting() {
+            buildIfNeed();
+            return fixedLengthRecordDesc;
+        }
+
+        private void buildIfNeed() {
+            if (fixedLengthRecordDesc != null) {
+                return;
+            }
+
+            /*
+             * 設定されているプロパティ名を対象に。
+             */
+
+            final ColumnDesc<Map<String, String>>[] cds = AbstractMapCsvLayout
+                    .toColumnDescs(columns);
+
+            final FixedLengthColumn[] a = columns
+                    .toArray(new FixedLengthColumn[columns.size()]);
+            fixedLengthRecordDesc = new FixedLengthRecordDesc<Map<String, String>>(
+                    cds, new AbstractMapCsvLayout.MapRecordType(), a);
+        }
+    }
+
+}
---
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/csv/MapFixedLengthReaderTest.java
@@ -0,0 +1,254 @@
+package jp.sourceforge.hotchpotch.coopie.csv;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.Map;
+
+import jp.sourceforge.hotchpotch.coopie.LoggerFactory;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.t2framework.commons.util.CollectionsUtil;
+
+public class MapFixedLengthReaderTest {
+
+    private static final Logger logger = LoggerFactory.getLogger();
+
+    /**
+     * ファイルヘッダがBeanのプロパティ名と同じ場合。
+     * 
+     * ※固定長ファイルでは、ヘッダがあっても大事に扱わない。
+     */
+    @Test
+    public void read1() throws Throwable {
+        // ## Arrange ##
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-1", "tsv");
+
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 5);
+                setup.column("ccc", 5, 12);
+                setup.column("bbb", 12, 20);
+            }
+        });
+
+        // ## Act ##
+        final CsvReader<Map<String, String>> csvReader = layout
+                .openReader(new InputStreamReader(is, "UTF-8"));
+
+        // ## Assert ##
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        MapCsvReaderTest.assertRead1(csvReader, bean);
+    }
+
+    /**
+     * ファイルヘッダがBeanのプロパティ名と異なる場合。
+     * ヘッダに何と書いてあろうとも、指定した文字数順に取り扱うこと。
+     * (CSVのときのように、ヘッダの順序に合わせて指定した順を変更しないこと)
+     * ※固定長ファイルでは、ヘッダがあっても大事に扱わない。
+     */
+    @Test
+    public void read2() throws Throwable {
+        // ## Arrange ##
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-2", "tsv");
+
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 5);
+                setup.column("ccc", 5, 12);
+                setup.column("bbb", 12, 30);
+            }
+        });
+
+        // ## Act ##
+        final CsvReader<Map<String, String>> csvReader = layout
+                .openReader(new InputStreamReader(is, "UTF-8"));
+
+        // ## Assert ##
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        MapCsvReaderTest.assertRead2(csvReader, bean);
+    }
+
+    /**
+     * ファイルヘッダが無い場合。
+     * 
+     * ※これが通常の固定長ファイル
+     */
+    @Test
+    public void read_noheader() throws Throwable {
+        // ## Arrange ##
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-3", "tsv");
+
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("ccc", 0, 6);
+                setup.column("aaa", 6, 12);
+                setup.column("bbb", 12, 20);
+            }
+        });
+
+        layout.setWithHeader(false);
+
+        // ## Act ##
+        final CsvReader<Map<String, String>> csvReader = layout
+                .openReader(new InputStreamReader(is, "UTF-8"));
+
+        // ## Assert ##
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        MapCsvReaderTest.assertReadNoheader(csvReader, bean);
+    }
+
+    /**
+     * 空白項目がある場合。
+     * 
+     * CSVでは""はnullとして扱い、" "は" "として扱うが、
+     * 固定長では""も" "もnullとする。
+     */
+    @Test
+    public void read3() throws Throwable {
+        // ## Arrange ##
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-4", "tsv");
+
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 7);
+                setup.column("bbb", 7, 14);
+                setup.column("ccc", 14, 20);
+            }
+        });
+
+        // ## Act ##
+        final CsvReader<Map<String, String>> csvReader = layout
+                .openReader(new InputStreamReader(is, "UTF-8"));
+
+        // ## Assert ##
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        assertRead3(csvReader, bean);
+    }
+
+    private static void assertRead3(
+            final CsvReader<Map<String, String>> csvReader,
+            final Map<String, String> bean) throws IOException {
+
+        assertEquals(true, csvReader.hasNext());
+        csvReader.read(bean);
+        logger.debug(bean.toString());
+        assertEquals("あ1", bean.get("aaa"));
+        assertEquals("い1", bean.get("bbb"));
+        //assertEquals(" ", bean.getCcc());
+        assertEquals(null, bean.get("ccc"));
+
+        assertEquals(true, csvReader.hasNext());
+        csvReader.read(bean);
+        logger.debug(bean.toString());
+        assertEquals(null, bean.get("aaa"));
+        assertEquals("い2", bean.get("bbb"));
+        assertEquals(null, bean.get("ccc"));
+
+        assertEquals(false, csvReader.hasNext());
+        csvReader.close();
+    }
+
+    /**
+     * 空ファイルの場合。
+     */
+    @Test
+    public void read_empty() throws Throwable {
+        // ## Arrange ##
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                // 空ファイルなので、ここは何でも良い
+                setup.column("aaa", 0, 7);
+                setup.column("bbb", 7, 14);
+                setup.column("ccc", 14, 20);
+            }
+        });
+
+        layout.setWithHeader(false);
+
+        // ## Act ##
+        final CsvReader<Map<String, String>> csvReader = layout
+                .openReader(new StringReader(""));
+
+        // ## Assert ##
+        assertEquals(false, csvReader.hasNext());
+
+        csvReader.close();
+    }
+
+    /**
+     * 空行がある場合。
+     * 
+     * 各要素を"" (null)として扱う。
+     */
+    @Test
+    public void read_empty_row() throws Throwable {
+        // ## Arrange ##
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-5", "tsv");
+
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 7);
+                setup.column("ccc", 7, 14);
+                setup.column("bbb", 14, 20);
+            }
+        });
+
+        // ## Act ##
+        final CsvReader<Map<String, String>> csvReader = layout
+                .openReader(new InputStreamReader(is, "UTF-8"));
+
+        // ## Assert ##
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        MapCsvReaderTest.assertReadEmptyRow(csvReader, bean);
+    }
+
+    /**
+     * setupしない列が入力ファイルに存在する場合は無視する。
+     */
+    @Test
+    public void read4() throws Throwable {
+        // ## Arrange ##
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-2", "tsv");
+
+        final MapFixedLengthLayout layout = new MapFixedLengthLayout();
+        layout.setupColumns(new SetupBlock<FixedLengthColumnSetup>() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 5);
+                setup.column("ccc", 5, 12);
+            }
+        });
+
+        // ## Act ##
+        final CsvReader<Map<String, String>> csvReader = layout
+                .openReader(new InputStreamReader(is, "UTF-8"));
+
+        // ## Assert ##
+        final Map<String, String> bean = CollectionsUtil.newHashMap();
+        MapCsvReaderTest.assertRead4(csvReader, bean);
+    }
+
+}
