baa8c40bddd684234105972ee4e5efb9e9b90884
manhole <manholex@gmail.com>
2011-08-06 14:36:14 +0000

    FixedLengthColumnSetupとFixedLengthRecordDescSetupの継承関係が逆だった。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@266 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/main/java/jp/sourceforge/hotchpotch/coopie/csv/FixedLengthColumnSetup.java
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/FixedLengthColumnSetup.java
@@ -1,9 +1,6 @@
 package jp.sourceforge.hotchpotch.coopie.csv;
 
-import jp.sourceforge.hotchpotch.coopie.csv.AbstractFixedLengthLayout.FixedLengthRecordDescSetup;
-
-public interface FixedLengthColumnSetup<T> extends
-        FixedLengthRecordDescSetup<T> {
+public interface FixedLengthColumnSetup {
 
     void column(String propertyName, int beginIndex, int endIndex);
 
