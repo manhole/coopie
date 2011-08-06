8886d71c55a7ef94835218bb129bc39ca2158ec0
manhole <manhole@ac04e864-1e22-0410-bbb8-8cc9166c4c4a>
2011-08-06 14:36:38 +0000

    固定長ファイル出力時に、項目順を指定する場合をテスト
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@271 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/test/java/jp/sourceforge/hotchpotch/coopie/csv/BeanFixedLengthWriterTest.java
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/csv/BeanFixedLengthWriterTest.java
@@ -18,6 +18,10 @@ public class BeanFixedLengthWriterTest {
 
     private static final Logger logger = LoggerFactory.getLogger();
 
+    /**
+     * 出力できること。
+     * データは右側に寄っていること。
+     */
     @Test
     public void write1() throws Throwable {
         // ## Arrange ##
@@ -57,6 +61,59 @@ public class BeanFixedLengthWriterTest {
                 "UTF-8"));
         assertEquals(expected, actual);
     }
+
+    /**
+     * カラム順を設定できること。
+     */
+    @Test
+    public void write2() throws Throwable {
+        // ## Arrange ##
+        final BeanFixedLengthLayout<AaaBean> layout = new BeanFixedLengthLayout<AaaBean>(
+                AaaBean.class);
+        layout.setupColumns(new FixedLengthColumnSetupBlock() {
+            @Override
+            public void setup(final FixedLengthColumnSetup setup) {
+                setup.column("aaa", 0, 5);
+                setup.column("ccc", 5, 12);
+                setup.column("bbb", 12, 20);
+            }
+        });
+
+        // ## Act ##
+        final StringWriter writer = new StringWriter();
+        final CsvWriter<AaaBean> csvWriter = layout.openWriter(writer);
+
+        final AaaBean bean = new AaaBean();
+        bean.setAaa("あ1");
+        bean.setBbb("い1");
+        bean.setCcc("う1");
+        csvWriter.write(bean);
+
+        bean.setAaa("あ2");
+        bean.setBbb("い2");
+        bean.setCcc("う2");
+        csvWriter.write(bean);
+
+        bean.setAaa("あ3");
+        bean.setBbb("い3");
+        bean.setCcc("う3");
+        csvWriter.write(bean);
+
+        csvWriter.close();
+
+        // ## Assert ##
+        final String actual = writer.toString();
+
+        final InputStream is = BeanFixedLengthReaderTest.getResourceAsStream(
+                "-1", "tsv");
+        final String expected = ReaderUtil.readText(new InputStreamReader(is,
+                "UTF-8"));
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * TODO 定義した長さよりも実際のデータが長い場合
+     */
 
     static InputStream getResourceAsStream(final String suffix, final String ext) {
         return ResourceUtil.getResourceAsStream(
