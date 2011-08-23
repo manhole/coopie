e273d9d27a678cbd843d50d719fd16c22c8cb505
manhole <manhole@ac04e864-1e22-0410-bbb8-8cc9166c4c4a>
2011-08-23 13:11:28 +0000

    StdOutBlockでsyserrもtrapできるようにした。あわせてTaskExecutableを導入。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@312 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/main/java/jp/sourceforge/hotchpotch/coopie/StdOutBlock.java
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/StdOutBlock.java
@@ -10,31 +10,66 @@ import org.t2framework.commons.util.task.Task;
  */
 public class StdOutBlock {
 
-    private PrintStream out = System.out;
-
-    public void setOut(final OutputStream os) {
-        out = new PrintStream(os, true);
+    public static Block out(final OutputStream os) {
+        final Block a = new Block();
+        a.out(os);
+        return a;
     }
 
-    public <V, E extends Throwable> V execute(final Task<V, E> task) throws E {
-        final PrintStream ps = out;
-        try {
-            final V ret = trap(task, ps);
-            return ret;
-        } finally {
-            ps.flush();
-        }
+    public static Block err(final OutputStream os) {
+        final Block a = new Block();
+        a.err(os);
+        return a;
     }
 
-    private <V, E extends Throwable> V trap(final Task<V, E> task,
-            final PrintStream printStream) throws E {
-        final PrintStream defaultOut = System.out;
-        System.setOut(printStream);
-        try {
-            return task.execute();
-        } finally {
-            System.setOut(defaultOut);
+    private StdOutBlock() {
+    }
+
+    public static class Block implements TaskExecutable {
+
+        private PrintStream out = System.out;
+        private PrintStream err = System.err;
+
+        public Block out(final OutputStream os) {
+            out = new PrintStream(os, true);
+            return this;
         }
+
+        public Block err(final OutputStream os) {
+            err = new PrintStream(os, true);
+            return this;
+        }
+
+        @Override
+        public <V, E extends Throwable> V execute(final Task<V, E> task)
+                throws E {
+            final PrintStream pOut = out;
+            final PrintStream pErr = err;
+            try {
+                final V ret = trap(task, pOut, pErr);
+                return ret;
+            } finally {
+                pOut.flush();
+            }
+        }
+
+        private <V, E extends Throwable> V trap(final Task<V, E> task,
+                final PrintStream pOut, final PrintStream pErr) throws E {
+            final PrintStream defaultOut = System.out;
+            final PrintStream defaultErr = System.err;
+            System.setOut(pOut);
+            try {
+                System.setErr(pErr);
+                try {
+                    return task.execute();
+                } finally {
+                    System.setErr(defaultErr);
+                }
+            } finally {
+                System.setOut(defaultOut);
+            }
+        }
+
     }
 
 }
