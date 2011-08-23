9d67be371e71cbbf9b4e4fa1379527e3a888a0cf
manhole <manhole@ac04e864-1e22-0410-bbb8-8cc9166c4c4a>
2011-08-23 13:10:47 +0000

    add: ContextClassLoaderBlock
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@301 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

---
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/ContextClassLoaderBlockTest.java
@@ -0,0 +1,46 @@
+package jp.sourceforge.hotchpotch.coopie;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Test;
+import org.t2framework.commons.util.task.Task;
+
+import com.sun.org.apache.bcel.internal.util.ClassLoader;
+
+public class ContextClassLoaderBlockTest {
+
+    /**
+     * 指定したClassLoaderを、ContextClassLoaderにセットすること。
+     */
+    @Test
+    public void test() throws Throwable {
+        // ## Arrange ##
+        final Object returns = new Object();
+        final AtomicBoolean called = new AtomicBoolean(false);
+        final ClassLoader cl = new ClassLoader();
+        final ContextClassLoaderBlock<Object, RuntimeException> block = ContextClassLoaderBlock
+                .with(cl);
+
+        assertNotSame(cl, Thread.currentThread().getContextClassLoader());
+
+        // ## Act ##
+        final Object ret = block.execute(new Task<Object, RuntimeException>() {
+            @Override
+            public Object execute() throws RuntimeException {
+                called.set(true);
+                assertSame(cl, Thread.currentThread().getContextClassLoader());
+                return returns;
+            }
+        });
+
+        // ## Assert ##
+        assertNotSame(cl, Thread.currentThread().getContextClassLoader());
+        assertSame(returns, ret);
+        assertEquals(true, called.get());
+    }
+
+}
