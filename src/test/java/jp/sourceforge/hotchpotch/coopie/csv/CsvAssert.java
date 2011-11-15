5f1cfb46ce5e55ff0244e0550defe2ba18de4c71
manhole <manhole@ac04e864-1e22-0410-bbb8-8cc9166c4c4a>
2011-11-15 07:06:16 +0000

    全件メモリに溜め込んでListAssertするのをやめ、1件ずつMapとして比較するようにした。 (他から使いやすくするため)
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@349 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/test/java/jp/sourceforge/hotchpotch/coopie/csv/CsvAssert.java
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/csv/CsvAssert.java
@@ -1,49 +1,68 @@
 package jp.sourceforge.hotchpotch.coopie.csv;
 
 import java.io.File;
-import java.util.List;
+import java.io.Reader;
 import java.util.Map;
 
 import jp.sourceforge.hotchpotch.coopie.FileOperation;
 import jp.sourceforge.hotchpotch.coopie.IOUtil;
-import junitx.framework.ListAssert;
 
-import org.t2framework.commons.util.CollectionsUtil;
+import org.junit.Assert;
 
 public class CsvAssert {
 
     private final FileOperation files_ = new FileOperation();
 
+    /**
+     * CSVとして等しいかassertします。
+     * 区切り文字やクォート有無の違いについては対象外です。
+     */
     public void assertCsvEquals(final File expectedFile, final File actualFile) {
-        final List<Map<String, String>> actList;
-        {
-            final MapCsvLayout layout = new MapCsvLayout();
-            layout.setWithHeader(true);
-            final CsvReader<Map<String, String>> actualCsvReader = layout
-                    .openReader(files_.openBufferedReader(actualFile));
-            actList = readAll(actualCsvReader);
-        }
-
-        final List<Map<String, String>> exList;
-        {
-            final MapCsvLayout layout = new MapCsvLayout();
-            layout.setWithHeader(true);
-
-            final CsvReader<Map<String, String>> expectedCsvReader = layout
-                    .openReader(files_.openBufferedReader(expectedFile));
-            exList = readAll(expectedCsvReader);
-        }
-        ListAssert.assertEquals(exList, actList);
+        final Reader actualReader = files_.openBufferedReader(actualFile);
+        final Reader expectedReader = files_.openBufferedReader(expectedFile);
+        assertCsvEquals(expectedReader, actualReader);
     }
 
-    private <T> List<T> readAll(final CsvReader<T> reader) {
-        final List<T> list = CollectionsUtil.newArrayList();
-        while (reader.hasNext()) {
-            final T bean = reader.read();
-            list.add(bean);
+    /**
+     * CSVとして等しいかassertします。
+     * 区切り文字やクォート有無の違いについては対象外です。
+     */
+    public void assertCsvEquals(final Reader expectedReader,
+            final Reader actualReader) {
+        try {
+            final CsvReader<Map<String, String>> actualCsvReader = openMapReader(actualReader);
+            final CsvReader<Map<String, String>> expectedCsvReader = openMapReader(expectedReader);
+            assertCsvEquals(expectedCsvReader, actualCsvReader);
+        } finally {
+            IOUtil.closeNoException(expectedReader);
+            IOUtil.closeNoException(actualReader);
         }
-        IOUtil.closeNoException(reader);
-        return list;
+    }
+
+    private CsvReader<Map<String, String>> openMapReader(
+            final Reader actualReader) {
+        final MapCsvLayout layout = new MapCsvLayout();
+        layout.setWithHeader(true);
+        final CsvReader<Map<String, String>> csvReader = layout
+                .openReader(actualReader);
+        return csvReader;
+    }
+
+    private void assertCsvEquals(
+            final CsvReader<Map<String, String>> expectedCsvReader,
+            final CsvReader<Map<String, String>> actualCsvReader) {
+
+        while (expectedCsvReader.hasNext() && actualCsvReader.hasNext()) {
+            final Map<String, String> exp = expectedCsvReader.read();
+            final Map<String, String> act = actualCsvReader.read();
+            Assert.assertEquals(exp, act);
+        }
+        if (expectedCsvReader.hasNext() || actualCsvReader.hasNext()) {
+            throw new AssertionError("different size");
+        }
+
+        IOUtil.closeNoException(expectedCsvReader);
+        IOUtil.closeNoException(actualCsvReader);
     }
 
 }
