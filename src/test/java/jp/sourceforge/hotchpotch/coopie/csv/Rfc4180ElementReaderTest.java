d6db6be2aaddbf4d2a69c371a05e20d049d94631
manhole <manholex@gmail.com>
2011-11-19 02:37:54 +0900

    OpenCSVの使用をやめた

--- src/test/java/jp/sourceforge/hotchpotch/coopie/csv/Rfc4180ElementReaderTest.java
+++ src/test/java/jp/sourceforge/hotchpotch/coopie/csv/Rfc4180ElementReaderTest.java
@@ -3,18 +3,18 @@ package jp.sourceforge.hotchpotch.coopie.csv;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
-import au.com.bytecode.opencsv.CSVReader;
-
-public class OpenCsvReaderAdapterTest extends ElementReaderTest {
+public class Rfc4180ElementReaderTest extends ElementReaderTest {
 
     @Override
     protected ElementReader constructTest1Reader() {
         final InputStreamReader reader = new InputStreamReader(
                 BeanCsvReaderTest.getResourceAsStream("-1", "tsv"),
                 Charset.forName("UTF-8"));
-        final CSVReader csvReader = new CSVReader(reader, CsvSetting.TAB,
-                CsvSetting.DOUBLE_QUOTE);
-        return new OpenCsvReaderAdapter(csvReader);
+        final Rfc4180Reader csvReader = new Rfc4180Reader();
+        csvReader.setElementSeparator(CsvSetting.TAB);
+        csvReader.setQuoteMark(CsvSetting.DOUBLE_QUOTE);
+        csvReader.open(reader);
+        return csvReader;
     }
 
 }
