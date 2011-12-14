d57d7f729f96a3c12aa3160a42f4462154567d6d
manhole <manholex@gmail.com>
2011-12-14 12:09:08 +0000

    comment
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@427 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/main/java/jp/sourceforge/hotchpotch/coopie/csv/ElementReader.java
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/ElementReader.java
@@ -4,8 +4,22 @@ import jp.sourceforge.hotchpotch.coopie.util.Closable;
 
 public interface ElementReader extends Closable {
 
+    /**
+     * レコード番号を返します。
+     * 初期値は0です。
+     * {@link #readRecord()}で1件目を取得した後は、1を返すようになります。
+     * 同様に、10件目を取得した後は10を返します。
+     * {@link #readRecord()}で最後まで読むと、それ以上大きい値を返さないようになります。
+     * 
+     * レコードが複数行に渡る可能性がある場合は、テキストの行番号とは異なる値となります。
+     * (テキストの行番号ではなく、データ番号)
+     */
     int getRecordNo();
 
+    /**
+     * 1レコードを読んで返します。
+     * 末端まで読んだ場合はnullを返すようになります。
+     */
     String[] readRecord();
 
 }
