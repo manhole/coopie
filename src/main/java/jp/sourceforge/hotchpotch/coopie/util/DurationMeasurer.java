965c1338442c9506b4e6911ad43c4ae62d5028c3
manhole <manholex@gmail.com>
2012-02-23 14:19:47 +0000

    add: Duration
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@485 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

---
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/util/DurationMeasurer.java
@@ -0,0 +1,51 @@
+package jp.sourceforge.hotchpotch.coopie.util;
+
+public class DurationMeasurer implements Duration {
+
+    private long begin_ = -1;
+    private long end_ = -1;
+
+    @Override
+    public long getBegin() {
+        return begin_;
+    }
+
+    @Override
+    public long getEnd() {
+        return end_;
+    }
+
+    @Override
+    public long getElapsed() {
+        if (-1 == begin_) {
+            throw new IllegalStateException();
+        }
+        if (-1 != end_) {
+            return end_ - begin_;
+        }
+        final long l = now() - begin_;
+        return l;
+    }
+
+    public void begin() {
+        if (-1 != begin_) {
+            throw new IllegalStateException();
+        }
+        begin_ = now();
+    }
+
+    public void end() {
+        if (-1 == begin_) {
+            throw new IllegalStateException();
+        }
+        if (-1 != end_) {
+            throw new IllegalStateException();
+        }
+        end_ = now();
+    }
+
+    protected long now() {
+        return System.currentTimeMillis();
+    }
+
+}
