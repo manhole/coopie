9c2cc33e5faf9f4fd98897786f1b7c6e406f3991
manhole <manholex@gmail.com>
2012-02-23 14:20:14 +0000

    primitive用を用意
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/hotchpotch/dev/trunk/coopie@493 ac04e864-1e22-0410-bbb8-8cc9166c4c4a

--- src/main/java/jp/sourceforge/hotchpotch/coopie/util/VarArgs.java
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/util/VarArgs.java
@@ -6,4 +6,36 @@ public class VarArgs {
         return ts;
     }
 
+    public static boolean[] a(final boolean... args) {
+        return args;
+    }
+
+    public static char[] a(final char... args) {
+        return args;
+    }
+
+    public static byte[] a(final byte... args) {
+        return args;
+    }
+
+    public static short[] a(final short... args) {
+        return args;
+    }
+
+    public static int[] a(final int... args) {
+        return args;
+    }
+
+    public static long[] a(final long... args) {
+        return args;
+    }
+
+    public static float[] a(final float... args) {
+        return args;
+    }
+
+    public static double[] a(final double... args) {
+        return args;
+    }
+
 }
