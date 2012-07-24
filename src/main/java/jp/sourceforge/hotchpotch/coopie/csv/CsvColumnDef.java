5110f03b97457bfd5db0c52195e041552fd58aab
manhole <manholex@gmail.com>
2012-07-25 00:32:06 +0900

    CsvColumnDefを初作成。Bean色が強くなってしまった。

---
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/CsvColumnDef.java
@@ -0,0 +1,30 @@
+package jp.sourceforge.hotchpotch.coopie.csv;
+
+import org.t2framework.commons.meta.PropertyDesc;
+
+/*
+ * @CsvColumnと対になる
+ */
+public interface CsvColumnDef<BEAN> {
+
+    String getLabel();
+
+    void setLabel(String label);
+
+    int getOrder();
+
+    void setOrder(int order);
+
+    Converter<?, ?> getConverter();
+
+    void setConverter(Converter<?, ?> converter);
+
+    Class<?> getPropertyType();
+
+    void setPropertyType(Class<?> propertyType);
+
+    PropertyDesc<BEAN> getPropertyDesc();
+
+    void setPropertyDesc(PropertyDesc<BEAN> propertyDesc);
+
+}
