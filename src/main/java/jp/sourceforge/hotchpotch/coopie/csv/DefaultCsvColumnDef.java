e66a59e0d0d4486b6ca6ca85dcd2b7e0a3f4e63e
manhole <manholex@gmail.com>
2012-10-03 02:07:15 +0900

    DefaultCsvColumnDef,DefaultCsvColumnsDefを独立したファイルに

---
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/DefaultCsvColumnDef.java
@@ -0,0 +1,106 @@
+package jp.sourceforge.hotchpotch.coopie.csv;
+
+import org.t2framework.commons.meta.PropertyDesc;
+import org.t2framework.commons.util.StringUtil;
+
+class DefaultCsvColumnDef implements CsvColumnDef, Comparable<CsvColumnDef> {
+
+    private String label_;
+    private String propertyName_;
+    private Class<?> propertyType_;
+
+    private int order_;
+    private Converter<?, ?> converter_ = PassthroughStringConverter
+            .getInstance();
+
+    private ColumnName columnName_;
+
+    public void setup(final CsvColumn column, final PropertyDesc pd) {
+        if (StringUtil.isBlank(column.label())) {
+            setLabel(pd.getPropertyName());
+        } else {
+            setLabel(column.label());
+        }
+        setOrder(column.order());
+        setPropertyName(pd.getPropertyName());
+        setPropertyType(pd.getPropertyType());
+    }
+
+    public void setup(final PropertyDesc pd) {
+        final String propertyName = pd.getPropertyName();
+        setLabel(propertyName);
+        setPropertyName(pd.getPropertyName());
+        setPropertyType(pd.getPropertyType());
+    }
+
+    @Override
+    public String getLabel() {
+        return label_;
+    }
+
+    @Override
+    public void setLabel(final String label) {
+        label_ = label;
+    }
+
+    @Override
+    public int getOrder() {
+        return order_;
+    }
+
+    @Override
+    public void setOrder(final int order) {
+        order_ = order;
+    }
+
+    @Override
+    public Converter<?, ?> getConverter() {
+        return converter_;
+    }
+
+    @Override
+    public void setConverter(final Converter<?, ?> converter) {
+        converter_ = converter;
+    }
+
+    @Override
+    public String getPropertyName() {
+        return propertyName_;
+    }
+
+    public void setPropertyName(final String propertyName) {
+        propertyName_ = propertyName;
+    }
+
+    @Override
+    public Class<?> getPropertyType() {
+        return propertyType_;
+    }
+
+    public void setPropertyType(final Class<?> propertyType) {
+        propertyType_ = propertyType;
+    }
+
+    @Override
+    public int compareTo(final CsvColumnDef o) {
+        // orderが小さい方を左側に
+        final int ret = getOrder() - o.getOrder();
+        return ret;
+    }
+
+    @Override
+    public ColumnName getColumnName() {
+        if (columnName_ == null) {
+            final SimpleColumnName columnName = new SimpleColumnName();
+            columnName.setName(getPropertyName());
+            columnName.setLabel(getLabel());
+            columnName_ = columnName;
+        }
+        return columnName_;
+    }
+
+    public void setColumnName(final ColumnName columnName) {
+        columnName_ = columnName;
+    }
+
+}
---
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/DefaultCsvColumnsDef.java
@@ -0,0 +1,71 @@
+package jp.sourceforge.hotchpotch.coopie.csv;
+
+import java.util.List;
+
+import org.t2framework.commons.meta.PropertyDesc;
+import org.t2framework.commons.util.CollectionsUtil;
+import org.t2framework.commons.util.StringUtil;
+
+class DefaultCsvColumnsDef implements CsvColumnsDef {
+
+    private String propertyName_;
+    private Class<?> propertyType_;
+    private Converter<?, ?> converter_ = PassthroughStringConverter
+            .getInstance();
+    private final List<CsvColumnDef> columnDefs_ = CollectionsUtil
+            .newArrayList();
+
+    public void setup(final CsvColumns columns, final PropertyDesc pd) {
+        for (final CsvColumn column : columns.value()) {
+            final DefaultCsvColumnDef columnDef = new DefaultCsvColumnDef();
+            if (StringUtil.isBlank(column.label())) {
+                columnDef.setLabel(pd.getPropertyName());
+            } else {
+                columnDef.setLabel(column.label());
+            }
+            columnDef.setOrder(column.order());
+            columnDef.setColumnName(new SimpleColumnName(columnDef.getLabel()));
+            addColumnDef(columnDef);
+        }
+        setPropertyName(pd.getPropertyName());
+        setPropertyType(pd.getPropertyType());
+    }
+
+    @Override
+    public List<CsvColumnDef> getColumnDefs() {
+        return columnDefs_;
+    }
+
+    public void addColumnDef(final CsvColumnDef columnDef) {
+        columnDefs_.add(columnDef);
+    }
+
+    @Override
+    public Converter<?, ?> getConverter() {
+        return converter_;
+    }
+
+    @Override
+    public void setConverter(final Converter<?, ?> converter) {
+        converter_ = converter;
+    }
+
+    @Override
+    public String getPropertyName() {
+        return propertyName_;
+    }
+
+    public void setPropertyName(final String propertyName) {
+        propertyName_ = propertyName;
+    }
+
+    @Override
+    public Class<?> getPropertyType() {
+        return propertyType_;
+    }
+
+    public void setPropertyType(final Class<?> propertyType) {
+        propertyType_ = propertyType;
+    }
+
+}
