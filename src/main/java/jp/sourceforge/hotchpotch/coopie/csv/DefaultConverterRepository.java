c1969864ca6931402069fdca640e7c0a347b6de1
manhole <manholex@gmail.com>
2012-11-01 14:05:57 +0900

    ConverterKeyを設けて、property型だけでなく外部型(といってもStringだけだけれど)も合わせてkeyとして扱うようにした。

--- src/main/java/jp/sourceforge/hotchpotch/coopie/csv/DefaultConverterRepository.java
+++ src/main/java/jp/sourceforge/hotchpotch/coopie/csv/DefaultConverterRepository.java
@@ -1,14 +1,21 @@
 package jp.sourceforge.hotchpotch.coopie.csv;
 
+import static jp.sourceforge.hotchpotch.coopie.util.VarArgs.a;
+
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.t2framework.commons.util.CollectionsUtil;
 
 public class DefaultConverterRepository implements ConverterRepository {
 
-    private final Map<Class, Converter> propertyTypeMap_ = CollectionsUtil
+    /*
+     * キーは、Java側プロパティの型
+     */
+    private final Map<ConverterKey, Converter> propertyTypeMap_ = CollectionsUtil
             .newHashMap();
 
     /*
@@ -27,7 +34,8 @@ public class DefaultConverterRepository implements ConverterRepository {
     }
 
     private Converter _detectByType(final Class<?> propertyType) {
-        final Converter converter = propertyTypeMap_.get(propertyType);
+        final Converter converter = propertyTypeMap_.get(new ConverterKey(
+                propertyType, a(String.class)));
         return converter;
     }
 
@@ -41,13 +49,59 @@ public class DefaultConverterRepository implements ConverterRepository {
                 if (Converter.class.isAssignableFrom((Class) rawType)) {
                     final Type[] actualTypeArguments = pType
                             .getActualTypeArguments();
-                    final Type propertyType = actualTypeArguments[0];
-                    final Type outerType = actualTypeArguments[1];
-                    propertyTypeMap_.put((Class) propertyType, converter);
+                    // XXX 型チェックが必要かも
+                    final Class propertyType = (Class) actualTypeArguments[0];
+                    final Class outerType = (Class) actualTypeArguments[1];
+                    propertyTypeMap_.put(new ConverterKey(propertyType,
+                            a(outerType)), converter);
                     return;
                 }
             }
         }
+    }
+
+    private static class ConverterKey {
+
+        private final Class<?> propertyType_;
+        private final Class<?>[] outerTypes_;
+        private final int hashCode_;
+
+        ConverterKey(final Class<?> propertyType, final Class<?>[] outerTypes) {
+            propertyType_ = propertyType;
+            outerTypes_ = outerTypes;
+
+            final int coefficient = 37;
+            int c = 17;
+            c = c * coefficient + propertyType.hashCode();
+            for (final Class<?> outerType : outerTypes) {
+                c = c * coefficient + outerType.hashCode();
+            }
+            hashCode_ = c;
+        }
+
+        @Override
+        public int hashCode() {
+            return hashCode_;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof ConverterKey)) {
+                return false;
+            }
+            final ConverterKey another = (ConverterKey) obj;
+            if (!propertyType_.equals(another.propertyType_)) {
+                return false;
+            }
+            if (!Arrays.equals(outerTypes_, another.outerTypes_)) {
+                return false;
+            }
+            return true;
+        }
+
     }
 
 }
